97acd1d52e4b13e387812e4c751d4e6e1b1a13ba
LucasJDove <86402904+Lucas-Uniqodo@users.noreply.github.com>
2021-07-26 10:23:34 +0100

    Revert "Merge branch 'main' into Lucas"
    This reverts commit 8d9e7abae3729e0d2b4d7d0e6e5cfdc0a4dc260a, reversing
    changes made to ca58ec31e3638713412e66c210e1c5efe9366f52.

--- initialiseDB.js
+++ initialiseDB.js
@@ -1,27 +1,32 @@
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require("sqlite3").verbose();
 /**
- * This code creates the tables for the e-commerce assignment
+ * This code creates the tables for the Restaurants assignment
  */
 function initialise() {
-    // connect to a database named omniverse.sqlite
-    const db = new sqlite3.Database('./omniverse.sqlite');
-    freezeTableName: true;
-    try {
-        db.serialize(function () { // serialize means execute one statement at a time
-            console.log('starting table creation');
-            // delete tables if they already exist
-            db.run("DROP TABLE IF EXISTS Category");
-            db.run("DROP TABLE IF EXISTS Item");
-            // create new, empty tables with specific columns and column types
-            db.run("CREATE TABLE Category (id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)");
-            db.run("CREATE TABLE Item(id Integer Primary Key Autoincrement, title STRING, price FLOAT, image STRING,FOREIGN KEY (CategoryId) REFERENCES Category(id))");
-        });
-    } finally {
-        // very important to always close database connections
-        // else could lead to memory leaks
-        db.close();
-        console.log('table creation complete - connection closed');
-    }
+	// connect to a database named restaurants.sqlite
+	const db = new sqlite3.Database("./omniverse.sqlite");
+	try {
+		db.serialize(function () {
+			// serialize means execute one statement at a time
+			console.log("starting table creation");
+			// delete tables if they already exist
+			db.run("DROP TABLE IF EXISTS Categories");
+			db.run("DROP TABLE IF EXISTS Items");
+			// create new, empty tables with specific columns and column types
+			db.run(
+				"CREATE TABLE Categories (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)"
+			);
+			db.run(
+				"CREATE TABLE Items (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, price FLOAT, description TEXT, image TEXT, categoryId INT, FOREIGN KEY (categoryId) REFERENCES Categories(id))"
+			);
+			// TODO - do the same for the other tables
+		});
+	} finally {
+		// very important to always close database connections
+		// else could lead to memory leaks
+		db.close();
+		console.log("table creation complete - connection closed");
+	}
 }
 module.exports = initialise;
 // local testing (comment out if running tests from jest)
