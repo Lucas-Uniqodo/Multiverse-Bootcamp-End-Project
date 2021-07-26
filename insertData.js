d9a612c78749e555856b4313fd60c7184c50d3e1
LucasJDove <86402904+Lucas-Uniqodo@users.noreply.github.com>
2021-07-26 10:35:35 +0100

    previous pull request reverted my DB changes

--- insertData.js
+++ insertData.js
@@ -1,60 +1,49 @@
 const sqlite3 = require('sqlite3').verbose();
 const fsp = require('fs').promises; // Node.js file system module with promises
-
-async function load() { // returns a promise of an array
-    const rawData = await fsp.readFile('./items.json');
-    const restaurantsArray = (JSON.parse(String(rawData)));
-    return restaurantsArray;
-}
-function insert(restaurantsArray) {
+/**
+This code illustrates how to load JSON data into an array.
+*/
+async function loadAndPrint() {
     const db = new sqlite3.Database('./omniverse.sqlite');
-
+    // wait for the json file to be read
     try {
+        const rawData = await fsp.readFile('./items.json');
+        // convert the file data into JS objects (arrays)
+        const categoriessArray = (JSON.parse(String(rawData)));
+
+
         db.serialize(function () {
 
-            let menuCounter = 1;
-
-            for (let i = 0; i < restaurantsArray.length; i++) {
-
-                const currentRestaurant = restaurantsArray[i];
-
-                let stmt;
-
+            //this type of for loop allows for easy reference to both the index, and the element itself
+            for (const [categoryIndex, category] of categoriessArray.entries()) {
+                let stmt
+                
                 try {
-                    stmt = db.prepare(`INSERT INTO Category (name) VALUES (?)`);
-                    stmt.run(currentRestaurant.name);
+                    stmt = db.prepare('INSERT INTO Categories (name) VALUES (?)')
+                    stmt.run(category.name)
+                    
                 } finally {
                     stmt.finalize();
                 }
 
-                for (j = 0; j < currentRestaurant.item.length; j++) {
-                    const currentMenu = currentRestaurant.item[j];
+                category.items.forEach(item => {
+
                     try {
-                        stmt = db.prepare(`INSERT INTO Item (title, price, image) VALUES (?, ?, ?)`);
-                        stmt.run(currentMenu.title, i + 1);
+                        stmt = db.prepare('INSERT INTO Items (title, price, description, image, categoryId) VALUES (?, ?, ?, ?, ?)')
+                        stmt.run(item.title, item.price, item.description, item.image, categoryIndex+1)
+                        
                     } finally {
                         stmt.finalize();
                     }
+                });
+            };
 
-                    menuCounter++;
-                }
-            }
         });
-    } finally {
-        db.close();
-        console.log('database closed');
+    } catch (error) {
+        // if we get here, our file read has failed
+        console.error('problem reading the file'+ error);
     }
 }
-module.exports = { load, insert }
+// main flow
 
-// local testing (comment out if running tests from jest)
-console.log('starting populate');
-load()
-    .then(restaurantsArray => {
-        console.log("data loaded");
-        insert(restaurantsArray);
-        console.log("inserts complete");
-    })
-    .catch(err => {
-        console.error("data could not be loaded"+err);
-    })
+module.exports = loadAndPrint()
